0f32513e30f520c25abf68e00b1c66365e649145
Minh Le <duyminh.le310@gmail.com>
2022-11-04 16:39:46 -0400

    Add data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,37 @@
+import { NextPage } from "next";
+import Head from "next/head"
+import { FunctionComponent } from "react";
+import favicon from '../assets/images/favicon-32x32.png';
 
+import dataJSON from '../data.json';
 
-export default function Home() {
+const Home:NextPage<{data:any}> = ({data}) =>{
+  
+  
   return (
-    <></>
+    <>
+      <Head>
+        <meta charSet="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/> 
+        <link rel="icon" type="image/png" sizes="32x32" href={favicon.src}/>
+        <title>Frontend Mentor | Interactive comments section</title>
+      </Head>
+      {
+        data.comments.map((comment:object) => (
+          console.log(comment)
+        ))
+      }
+    </>
   )
+}
+
+export default Home;
+
+
+export async function getServerSideProps() {
+  return {
+    props: {
+      data: dataJSON
+    }
+  }
 }
