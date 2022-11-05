f559d5524689eea810cb946d0fab784f3c2db30f
Minh Le <duyminh.le310@gmail.com>
2022-11-05 16:40:40 -0400

    Change Color of plus and minus Score

--- components/Comment/index.tsx
+++ components/Comment/index.tsx
@@ -3,11 +3,12 @@ import styles from './Comment.module.scss';
 import Image from "next/image";
 
 
-import iconPlus from '../../assets/images/icon-plus.svg';
-import iconMinus from '../../assets/images/icon-minus.svg';
-import iconReply from '../../assets/images/icon-reply.svg';
+import IconPlus from '../../assets/images/icon-plus.svg';
+import IconMinus from '../../assets/images/icon-minus.svg';
+import IconReply from '../../assets/images/icon-reply.svg';
 
 import { CommentComponent } from "../../TypesAndInterfaces";
+import { FunctionComponent, useEffect, useState } from 'react';
 
 
 const Comment:CommentComponent = ({avatarURL, authorName, createdAt, content, score, replyingTo}) => {
@@ -24,37 +25,26 @@ const Comment:CommentComponent = ({avatarURL, authorName, createdAt, content, sc
                         height="32"
                     />
                 </div>
-                <span className={styles.authorName}>{authorName}</span>
-                <span className={styles.createdAt}>{createdAt}</span>
+                <span className={styles.authorName}>
+                    {authorName}
+                </span>
+                <span className={styles.createdAt}>
+                    {createdAt}
+                </span>
             </div>
             <div className={styles.content}>
-                {
-                    content
-                }
+                {content}
             </div>
             <div className={styles.footer}>
                 <div className={styles.score}>
-                    <button>
-                        <Image
-                            src={iconPlus}
-                            alt="add score"
-                        />
-                    </button>
+                    <Button type="PLUS"/>
                     <span>{score}</span>
-                    <button>
-                        <Image
-                            src={iconMinus}
-                            alt="minus score"
-                        />
-                    </button>
+                    <Button type="MINUS"/>
                 </div>
                 <div className={styles.functions}>
                     <div className={styles.reply}>
                         <div className={styles.replyIcon}>
-                            <Image 
-                                src={iconReply}
-                                alt="Reply icon"
-                            />
+                            <IconReply/>
                         </div>
                         <span>Reply</span>
                     </div>
@@ -66,3 +56,32 @@ const Comment:CommentComponent = ({avatarURL, authorName, createdAt, content, sc
 }
 
 export default Comment;
+
+
+/**************************************/
+
+const Button:FunctionComponent<{type:"PLUS"|"MINUS"}> = ({type, ...props}) => {
+    const [hover, setHover] = useState(false);
+
+    useEffect(() => {
+        console.log(hover);
+    }, [hover]);
+
+    return (
+        <button
+            {...props}
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+            >
+            {
+                type === 'PLUS'? 
+                    <IconPlus
+                        fill={hover? '#5357B6' : '#C5C6EF'}
+                    /> : 
+                    <IconMinus
+                        fill={hover? '#5357B6' : '#C5C6EF'}
+                    /> 
+            }
+        </button>
+    )
+}
