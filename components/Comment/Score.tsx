d65cd3b03072755951e6821986485570d463e297
Minh Le <duyminh.le310@gmail.com>
2022-11-23 22:35:53 -0500

    Disable Upvote/Downvote for non-user

--- components/Comment/Score.tsx
+++ components/Comment/Score.tsx
@@ -7,25 +7,33 @@ import { DataContextType } from "../../TypesAndInterfaces";
 
 import IconPlus from '../../assets/images/icon-plus.svg';
 import IconMinus from '../../assets/images/icon-minus.svg';
+import { useSession } from "next-auth/react";
 
 type ScoreType = FunctionComponent<{type:'THREAD'|'REPLY', id:string, parentID?:string, score:number, upvote: boolean, downvote: boolean}>  
 
 const Score:ScoreType = ({type, id, parentID, score, upvote, downvote}) => {
     const {vote} = useContext(DataContext) as DataContextType;
 
+    const {data: session} = useSession();
+
+
     return (
         <div className={styles.score}>
             <Button type="UPVOTE"
-                highlight={upvote}
+                highlight={session? upvote: false}
                 onClick={() => {
+                    if(!session) return;
+
                     if(!upvote) 
                         vote('UPVOTE', type, id, parentID);
                 }}
             />
             <span>{score}</span>
             <Button type="DOWNVOTE"
-                highlight={downvote}
+                highlight={session? downvote: false}
                 onClick={() => {
+                    if(!session) return;
+                    
                     if(!downvote)                    
                         vote('DOWNVOTE', type, id, parentID);
                 }}
