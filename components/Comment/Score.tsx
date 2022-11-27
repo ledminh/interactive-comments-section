677acfc22e221d0d1e08563aa362a4cd8ca30d8b
Minh Le <duyminh.le310@gmail.com>
2022-11-27 02:11:37 -0500

    Fix HTML Error

--- components/Comment/Score.tsx
+++ components/Comment/Score.tsx
@@ -51,7 +51,7 @@ const Button:PlusMinusButtonType = ({type, highlight, ...props}) => {
             {...props}
             onMouseEnter={() => setHover(true)}
             onMouseLeave={() => setHover(false)}
-            ariel-label={type === 'UPVOTE'? 'up vote' : 'down vote'}
+            ariel-label={type === 'UPVOTE'? 'upvote' : 'downvote'}
             >
             {
                 type === 'UPVOTE'? 
